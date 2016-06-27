a70f0a10c24ef9c7da5eeb045bf1575bc3faacde
Stefano <stefano.forti92@gmail.com>
2016-06-27 12:42:25 +0200

    stats

--- src/edu/stefano/deccen/controls/PostManControl.java
+++ src/edu/stefano/deccen/controls/PostManControl.java
@@ -1,6 +1,7 @@
 package edu.stefano.deccen.controls;
 
 import edu.stefano.deccen.centralities.AbstractDeccenCD;
+import java.util.ArrayList;
 import peersim.config.Configuration;
 import peersim.core.CommonState;
 import peersim.core.Control;
@@ -11,21 +12,32 @@ public class PostManControl implements Control {
 
     private static final String PAR_PROT = "deccen";
     private final int pid;
+    private final ArrayList<Integer> convergedNodes;
 
     public PostManControl(String prefix) {
         pid = Configuration.getPid(prefix + "." + PAR_PROT);
+        convergedNodes = new ArrayList<>();
     }
 
     @Override
     public boolean execute() {
         int emptyBoxes = 0;
+        boolean converged = true;
+        
         for (int i = 0; i < Network.size(); i++) {
             Node n = Network.get(i);
             AbstractDeccenCD prot = (AbstractDeccenCD) n.getProtocol(pid);
-            if (!prot.sendAll(n, pid)) {
+            if (converged = !prot.sendAll(n, pid)) {
                 emptyBoxes++;
+                if (CommonState.getTime() != 1 && !convergedNodes.contains(i)){
+                    System.out.println("Node " + i + " converged at cycle " + CommonState.getIntTime() + "!");
+                    convergedNodes.add(i);
+                }
             }
+            
         }
+        
+
 
         if (emptyBoxes == Network.size() && CommonState.getIntTime() != 1) {
             System.out.println("No more messages to deliver!");
